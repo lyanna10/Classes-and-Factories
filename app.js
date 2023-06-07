9fc9ce601dc395b1307a0f1f7514c3fc9be127a3
Lyann10 <glody.albadose@gmail.com>
2023-06-07 15:22:04 -0500

    created Timmy's story

--- app.js
+++ app.js
@@ -39,3 +39,122 @@ console.log(returnHamster.price);
 returnHamster.wheelRun();
 returnHamster.eatFood();
 returnHamster.getPrice();
+
+/**Create a Person class with the following requirements met:
+
+Attributes:
+
+name - set name from a parameter in the constructor method.
+age- initially 0
+height - initially 0
+weight - initially 0
+mood - integer starting at 0 initially
+hamsters - empty array initially
+bankAccount - initially set to 0
+Methods:
+
+getName() - returns name.
+getAge() - returns age.
+getWeight() - returns weight.
+greet() - logs a message with person's name.
+eat() - increment weight, increment mood.
+exercise() - decrement weight.
+ageUp() - increment age, increment height, increment weight, decrement mood, increment bankAccount by 10 (birthday money).
+buyHamster(hamster) - push the hamster object onto the hamsters array, increment mood by 10, decrement bankAccount by the value of the hamster (hint: use getPrice()). */
+class Person {
+    constructor(name) {
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = "";
+        this.bankAccount = 0
+    }
+
+    getName() {
+        console.log(`${this.name}`);
+    }
+
+    getAge() {
+        console.log(`${this.age}`);
+    }
+
+    getWeight() {
+        console.log(`${this.weight}`);
+    }
+
+    greet() {
+        console.log(`Hello ${this.name}, how are you doing?`);
+    }
+
+    eat() {
+        this.weight++;
+        this.mood++;
+    }
+
+    exercise() {
+        this.weight--;
+    }
+
+    ageUp() {
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount += 10;
+    }
+
+    buyHamster(hamster) {
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        this.bankAccount -= hamster.getPrice();
+    }
+}
+const personLog = new Person("Aaron");
+
+personLog.getName();
+personLog.greet();
+
+/**Feel free to update or add methods to automate some of these tasks.
+
+Instantiate a new Person named Timmy.
+Age Timmy five years.
+At this point, Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times to improve his mood.
+Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times.
+Age Timmy 9 more years.
+Create a Hamster named "Gus."
+Set Gus's owner to the string "Timmy."
+Have Timmy "buy" Gus.
+Age Timmy more 15 years.
+Have Timmy eat twice.
+Have Timmy exercise twice */
+const timmy = new Person("Timmy");
+for (let i = 0; i < 9; i++) {
+    timmy.ageUp();
+}
+
+for (let i = 0; i < 5; i++) {
+    timmy.eat();
+}
+
+for (let i = 0; i < 5; i++) {
+    timmy.exercise();
+}
+
+for (let i = 0; i < 9; i++) {
+    timmy.ageUp();
+}
+
+const gusTheHamster = new Person("Gus");
+gusTheHamster.owner = "Timmy";
+
+for (let i = 0; i < 15; i++) {
+    timmy.ageUp();
+}
+
+timmy.eat();
+timmy.eat();
+
+timmy.exercise();
+timmy.exercise();
